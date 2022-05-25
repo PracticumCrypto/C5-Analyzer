1a31dff4cd1c43617d92ac3630c9dc47d19e897d
scorevictim <1031161009sean@gmail.com>
2022-05-25 15:57:25 -0700

    updated NET

--- FactorConstruct/functionsNET.py
+++ FactorConstruct/functionsNET.py
@@ -12,46 +12,42 @@ def getNET(largeCapSample: pd.DataFrame):
     """
     import numpy as np
     
-    # Get Address Growth
-    Address = largeCapSample.query("ActiveAddress > 0").loc[:, ['Date', 'ActiveAddress', 'Asset']] \
-        .pivot(index='Date',
-               columns='Asset',
-               values='ActiveAddress')\
-        .reset_index() \
-        .rename_axis(None,
-                     axis=1)
-
-    copy1 = Address.copy()
-    copy2 = copy1.shift()
-
-    Growth = copy1.copy()
-
+    Growth = largeCapSample.pivot(index='Date',
+                              columns='Asset',
+                              values='GrowthRate').reset_index().rename_axis(None,
+                                                                             axis=1)
     cols = largeCapSample.Asset.unique().tolist()
 
-    for i in cols:
-        Growth[i] = np.log(copy1[i]) - np.log(copy2[i])
-
     Growth['20Percentile'] = Growth[cols].quantile(q=0.2,
-                                                   numeric_only=True,
-                                                   axis=1)
+                                                numeric_only=True,
+                                                axis=1)
     Growth['80Percentile'] = Growth[cols].quantile(q=0.8,
-                                                   numeric_only=True,
-                                                   axis=1)
-    # Construct Network factor
-    Return = largeCapSample.loc[:, ['Date',
-                                    'Return',
-                                    'Asset']].pivot(index='Date',
-                                                    columns='Asset',
-                                                    values='Return').reset_index().rename_axis(None,
-                                                                                               axis=1)
+                                                numeric_only=True,
+                                                axis=1)
+    
+    Return = largeCapSample.pivot(index='Date',
+                              columns='Asset',
+                              values='Return').reset_index().rename_axis(None,
+                                                                         axis=1)
 
     NET = pd.DataFrame({'Date': []})
     NET['Date'] = Return.Date
-    NET['long'] = Return[Growth[cols].apply(lambda x: x >= Growth['80Percentile'])].mean(axis=1,
-                                                                                         numeric_only=True)
-    NET['short'] = Return[Growth[cols].apply(lambda x: x <= Growth['20Percentile'])].mean(axis=1,
-                                                                                          numeric_only=True)
-    NET.fillna(value=0, 
-               inplace=True)
+
+    # Lambda function disordered the columns, added additional step to sort columns in alphabetical order
+    long = Growth[cols].apply(lambda x: x >= Growth['80Percentile'])
+    long = long.reindex(sorted(long.columns), 
+                        axis=1)
+    NET['long'] = Return[long].mean(axis=1,
+                                    numeric_only=True)
+
+    short = Growth[cols].apply(lambda x: x <= Growth['20Percentile'])
+    short = short.reindex(sorted(short.columns), 
+                        axis=1)
+    NET['short'] = Return[short].mean(axis=1, 
+                                    numeric_only=True)
+
     NET['NET'] = NET['long'] - NET['short']
-    return NET.drop(columns=['short', 'long'])
+    NET.drop(columns=['short', 'long'])
+    NET = NET[['Date', 'NET']]
+
+    return NET
