cd32bed2597a0dd6b95164a112de1427d0bacbde
scorevictim <1031161009sean@gmail.com>
2022-05-25 15:55:52 -0700

    updated MOM

--- FactorConstruct/functionsMOM.py
+++ FactorConstruct/functionsMOM.py
@@ -10,44 +10,35 @@ def getMomFactor(largeCapSample: pd.DataFrame):
     Returns:
         momFactor (pd.DataFrame): Contains Momentum Factor
     """
-    returnAll = largeCapSample.loc[:, ['Date',
-                                   'Return',
-                                       'Asset']].pivot(index="Date",
-                                                       columns=["Asset"],
-                                                       values='Return').reset_index().rename_axis(None,
-                                                                                                  axis=1).sort_values('Date')
-    momMatrix = largeCapSample.loc[:, ['Date',
-                                       'week2_return',
-                                       'Asset']].pivot(index="Date",
-                                                       columns=["Asset"],
-                                                       values='week2_return').reset_index().rename_axis(None,
-                                                                                                        axis=1).sort_values('Date')
-    col = []
-    for colName in momMatrix.columns:
-        if colName not in ['Date']:
-            col.append(colName)
-    momMatrix[col].head()
+    # Pivot largeCapSample to Get Return Matrix
+    returnAll = largeCapSample.pivot(index="Date",
+                                     columns=["Asset"],
+                                     values='Return').reset_index().rename_axis(None,
+                                                                                axis=1).sort_values('Date')
+
+    # Pivot largeCapSample to Get Momentum Matrix(Past 2-Week Returns)
+    momMatrix = largeCapSample.pivot(index="Date",
+                                     columns=["Asset"],
+                                     values='week2_return').reset_index().rename_axis(None,
+                                                                                      axis=1).sort_values('Date')
+
+    # Calculate the Momentum Percentile
+    col = sorted(largeCapSample.Asset.unique())
 
     momPt = pd.DataFrame(columns=['MOM20p',
                                   'MOM80p'])
 
-    momPt['MOM20p'] = momMatrix[col].quantile(.2,
-                                              axis=1)
-    momPt['MOM80p'] = momMatrix[col].quantile(.8,
-                                              axis=1)
+    momPt['MOM20p'] = momMatrix[col].quantile(.2, axis=1)
+    momPt['MOM80p'] = momMatrix[col].quantile(.8, axis=1)
 
-    momLH = pd.DataFrame(columns=['HighMomentum',
-                                  'LowMomentum'])
-
-    MOM = pd.DataFrame(columns=['Date',
-                                'MOM'])
-    MOM['Date'] = returnAll['Date']
-    momLH['HighMomentum'] = returnAll[momMatrix[largeCapSample.Asset.unique()].apply(
-        lambda x: x >= momPt['MOM80p'])].mean(axis=1,
-                                              numeric_only=True)
-    momLH['LowMomentum'] = returnAll[momMatrix[largeCapSample.Asset.unique()].apply(
-        lambda x: x <= momPt['MOM20p'])].mean(axis=1,
-                                              numeric_only=True)
-    MOM['MOM'] = momLH['HighMomentum'] - momLH['LowMomentum']
-    momFactor = MOM
+    # Calculate MOM Factor
+    MOM = pd.DataFrame({'Date': []})
+    MOM['Date'] = returnAll.Date
+    MOM['HighMomentum'] = returnAll.drop("Date", axis=1)[momMatrix[sorted(largeCapSample.Asset.unique())].apply(
+        lambda x: x >= momPt['MOM80p'])].mean(axis=1, numeric_only=True)
+    MOM['LowMomentum'] = returnAll.drop("Date", axis=1)[momMatrix[sorted(largeCapSample.Asset.unique())].apply(
+        lambda x: x <= momPt['MOM20p'])].mean(axis=1, numeric_only=True)
+    MOM['MOM'] = MOM.HighMomentum - MOM.LowMomentum
+    momFactor = MOM.drop(columns=['HighMomentum', 'LowMomentum'])
     return momFactor
+
