139aaefaa39905984bb903e83d8ce4b637c43b70
scorevictim <1031161009sean@gmail.com>
2022-05-25 15:52:05 -0700

    updated SMB

--- FactorConstruct/functionsSMB.py
+++ FactorConstruct/functionsSMB.py
@@ -11,20 +11,17 @@ def getSMB(largeCapSample: pd.DataFrame):
         SMB_f (pd.DataFrame): Contains Market Factor
     """
     # Using quantile method to get the dynamic portfolios for top cap 20%, and bottom cap 20%
-    largeCap = largeCapSample.groupby(['Date']).apply(lambda
-                                                      x: x.nlargest(round(0.2*len(x)),
-                                                                    ['MarketCap'])).reset_index(drop=True)
+    largeCap = largeCapSample.groupby(['Date']).apply(lambda 
+                                                    x: x.nlargest(math.ceil(0.2*len(x)),
+                                                                ['MarketCap'])).reset_index(drop=True)
 
-    smallCap = largeCapSample.groupby(['Date']).apply(lambda
-                                                      x: x.nsmallest(round(0.2*len(x)),
-                                                                     ['MarketCap'])).reset_index(drop=True)
+    smallCap = largeCapSample.groupby(['Date']).apply(lambda 
+                                                    x: x.nsmallest(math.ceil(0.2*len(x)),
+                                                                ['MarketCap'])).reset_index(drop=True)
 
     # Using equal-weighted average method to get the SMB
-    largeport = largeCap.groupby(
-        'Date')['Return'].mean().reset_index(name='LargePortReturn')
-    smallport = smallCap.groupby(
-        'Date')['Return'].mean().reset_index(name='SmallPortReturn')
-    smallport['SMB'] = smallport['SmallPortReturn'] - \
-        largeport['LargePortReturn']
-    SMB_f = smallport[['Date', 'SMB']]
+    largeport = largeCap.groupby('Date')['Return'].mean().reset_index(name='LargePortReturn')
+    smallport = smallCap.groupby('Date')['Return'].mean().reset_index(name='SmallPortReturn')
+    smallport['SMB'] = smallport['SmallPortReturn'] - largeport['LargePortReturn']
+    SMB_f = smallport[['Date','SMB']]
     return SMB_f
